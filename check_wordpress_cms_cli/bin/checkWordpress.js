e2b5cd61d5817b1b4f8eda068cd07650044ce4d2
Slava Haikin <97975418+Vyachowski@users.noreply.github.com>
2024-03-17 11:46:12 +0200

    feat: add cli messsages

--- check_wordpress_cms_cli/bin/checkWordpress.js
+++ check_wordpress_cms_cli/bin/checkWordpress.js
@@ -8,6 +8,10 @@ program
   .version('1.0.0')
   .argument('<link>', 'link to the checking domain')
   .action((link) => {
-    console.log(isWordpress(link));
+    console.log(
+      ${isWordpress(link)}
+      ? `${link} was made with WordpPess`
+      : `${link} was NOT made with WordpPess`
+    );
   })
   .parse(process.argv);
