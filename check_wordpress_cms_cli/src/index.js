7c2170d7088458a1797cb957a35a52d41f55e4c4
Slava Haikin <97975418+Vyachowski@users.noreply.github.com>
2024-03-17 12:00:39 +0200

    feat: add program skeleton

---
+++ check_wordpress_cms_cli/src/index.js
@@ -0,0 +1,19 @@
+import { URL } from 'node:url';
+
+const normalizeDomain = (link) => new URL(link).hostname;
+
+const hasWpAdminPanel = () => {
+
+}
+
+const hasWpScripts = () => {
+
+}
+
+const hasSpecialDisallowRule = () => {
+
+}
+
+const hasSpecialCookie = () => {
+
+}
