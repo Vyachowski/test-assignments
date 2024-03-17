6dfae178d3e6c6770b69b18c130ed1a345e7cf3d
Slava Haikin <97975418+Vyachowski@users.noreply.github.com>
2024-03-17 12:41:44 +0200

    feat: add first check

--- check_wordpress_cms_cli/src/index.js
+++ check_wordpress_cms_cli/src/index.js
@@ -1,10 +1,23 @@
 import { URL } from 'node:url';
 
-const normalizeDomain = (link) => new URL(link).hostname;
+const normalizeLink = (link) => {
+  const normalizedLink = link.toLowerCase()
+    .startsWith('http')
+    ? new URL(link).hostname
+    : new URL(`http://${link}`).hostname
+  return `http://${normalizedLink}`;
+};
 
-const hasWpAdminPanel = () => {
+const normalizedLink = normalizeLink('https://vyachowski.wordpress.com');
 
+const hasWpAdminPanel = async () => {
+  const adminPageUrl = new URL('/wp-admin/', normalizedLink).href;
+  const result = await fetch(adminPageUrl);
+
+  return (result.status === 200);
 }
+
+console.log(await hasWpAdminPanel());
 
 const hasWpScripts = () => {
 
@@ -14,6 +27,6 @@ const hasSpecialDisallowRule = () => {
 
 }
 
-const hasSpecialCookie = () => {
+// const hasSpecialCookie = () => {
 
-}
+// }
