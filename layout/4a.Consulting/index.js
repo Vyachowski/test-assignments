7ab465d4ca9fcef90dd55466208625e021d11801
Slava Haikin <97975418+Vyachowski@users.noreply.github.com>
2024-04-05 01:50:24 +0300

    feat: add render functions

--- layout/4a.Consulting/index.js
+++ layout/4a.Consulting/index.js
@@ -41,29 +41,56 @@ const getSortedPrices = async (urlObj) => {
   }
 }
 
-const updateTimer = (visitTime) =>{
-  const TIMER_DURATION = 120_000;
-  const currentTime = new Date;
-  const passedTime = currentTime - visitTime;
-  const timeLeft = TIMER_DURATION - passedTime;
-  const minutesLeft = Math.floor(timeLeft / (1000 * 60));
-  const secondsLeft = Math.floor((timeLeft % (1000 * 60)) / 1000);
-
+const renderTimer = (minutesLeft, secondsLeft) =>{
   const hoursElement = document.querySelector('.discount-timer__time--hours strong');
   const minutesElement = document.querySelector('.discount-timer__time--minutes strong');
-  if (true) {}
+  const separatorElement = document.querySelector('.discount-timer__separator');
+  minutesLeft >= 10
+    ? hoursElement.textContent = String(minutesLeft)
+    : hoursElement.textContent = `0${String(minutesLeft)}`;
+  secondsLeft >= 10
+    ? minutesElement.textContent = String(secondsLeft)
+    : minutesElement.textContent = `0${String(secondsLeft)}`;
+
+  if (timeLeftInSeconds < 30) {
+    hoursElement.classList.add('warning-color');
+    minutesElement.classList.add('warning-color');
+    separatorElement.classList.add('warning-color');
+  }
 }
 
+const renderPopup = () => {
+  const bodyElement = document.querySelector('body');
+  const modalWindow = document.querySelector('.modal-window');
+  const closeButtonElement = document.querySelector('.modal__close-button');
 
+  bodyElement.classList.add('modal-open');
+  modalWindow.classList.add('modal-window--open');
+  closeButtonElement.addEventListener('click', () => {
+    bodyElement.classList.remove('modal-open');
+    modalWindow.classList.remove('modal-window--open');
+  })
+}
 
 const app = async () => {
-  console.log(userVisitTime.getTime());
   const pricesLink = new URL('/subscribe/list-test', 'https://t-pay.iqfit.app');
   const { isDiscount: discountPrices, isPopular: popupPrices, regular: regularPrices } = await getSortedPrices(pricesLink);
   const userVisitTime = new Date();
-  const timerId = setInterval(() => {
-
-  }, 1000)
+  renderDiscountPrices();
+  const intervalId = setInterval(() => {
+    const currentTime = new Date();
+    const passedTime = currentTime.getTime() - userVisitTime.getTime();
+    const TIMER_DURATION = 1_000;
+    timeLeftInSeconds = Math.floor((TIMER_DURATION - passedTime) / 1000);
+    const minutesLeft = Math.floor(timeLeftInSeconds / 60);
+    const secondsLeft = timeLeftInSeconds % 60;
+    renderTimer(minutesLeft, secondsLeft, intervalId);
+    if (timeLeftInSeconds === 0) {
+      clearInterval(intervalId);
+      renderPopup();
+      renderRegularPrices();
+    }
+  }, 100)
 }
 
 app();
